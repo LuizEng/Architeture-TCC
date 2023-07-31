9666549457c19f66471ed327472d94c0adf5c01a
Luiz <Luizh.eng98@gmail.com>
2023-07-31 08:12:25 -0300

    DEV-C#004 - Implementado CRUD de cadastros

---
+++ Versão Inicial - 01/Manager/Manager01/CadastroDescricaoValor_Form.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Manager01
+{
+    partial class CadastroDescricaoValor_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.grdDados = new System.Windows.Forms.DataGridView();
+            this.dtsDados = new System.Data.DataSet();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdDados)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtsDados)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSalvar);
+            this.panel1.Controls.Add(this.btnAlterar);
+            this.panel1.Controls.Add(this.btnIncluir);
+            this.panel1.Controls.Add(this.btnVoltar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(572, 32);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(410, 4);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 4;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(84, 4);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 3;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(3, 4);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.btnIncluir.TabIndex = 2;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.Location = new System.Drawing.Point(490, 4);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(75, 23);
+            this.btnVoltar.TabIndex = 0;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = true;
+            this.btnVoltar.Click += new System.EventHandler(this.btnVoltar_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtValor);
+            this.panel2.Controls.Add(this.txtDescricao);
+            this.panel2.Controls.Add(this.lblValor);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 32);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(572, 351);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtValor
+            // 
+            this.txtValor.Enabled = false;
+            this.txtValor.Location = new System.Drawing.Point(76, 52);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(285, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Enabled = false;
+            this.txtDescricao.Location = new System.Drawing.Point(76, 16);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(285, 20);
+            this.txtDescricao.TabIndex = 3;
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Location = new System.Drawing.Point(36, 55);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(34, 13);
+            this.lblValor.TabIndex = 2;
+            this.lblValor.Text = "Valor:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Descrição:";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.grdDados);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(0, 121);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(572, 262);
+            this.panel3.TabIndex = 2;
+            // 
+            // grdDados
+            // 
+            this.grdDados.AllowUserToAddRows = false;
+            this.grdDados.AllowUserToDeleteRows = false;
+            this.grdDados.AllowUserToOrderColumns = true;
+            this.grdDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdDados.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdDados.Location = new System.Drawing.Point(0, 0);
+            this.grdDados.Name = "grdDados";
+            this.grdDados.ReadOnly = true;
+            this.grdDados.Size = new System.Drawing.Size(572, 262);
+            this.grdDados.TabIndex = 0;
+            this.grdDados.SelectionChanged += new System.EventHandler(this.grdDados_SelectionChanged);
+            // 
+            // dtsDados
+            // 
+            this.dtsDados.DataSetName = "NewDataSet";
+            // 
+            // CadastroDescricaoValor_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 383);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "CadastroDescricaoValor_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CadastroDescricaoValor_Form";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdDados)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtsDados)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.DataGridView grdDados;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Data.DataSet dtsDados;
+    }
+}
---
+++ Versão Inicial - 01/Manager/Manager01/CadastroDescricaoValor_Form.cs
@@ -0,0 +1,200 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Utilities.Collections;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manager01
+{
+    enum TipoAcao
+    {
+        taIncluindo,
+        taAlterando,
+        taBrowse
+    }
+    public enum TipoCadastro
+    {
+        tcServico,
+        tcAtendimento,
+        tcPagamento
+    }
+
+    public partial class CadastroDescricaoValor_Form : Form
+    {
+        private TipoCadastro tipoCadastro;
+        private MySqlDataAdapter dataAdapter;
+        private BindingSource bindingSourceCadastro = new BindingSource();
+        private DataSet dataSet = new DataSet();
+        private String tabela;
+        private TipoAcao tipoAcao;
+
+        public CadastroDescricaoValor_Form(TipoCadastro tipoCadastro)
+        {
+            InitializeComponent();
+            this.tipoCadastro = tipoCadastro;
+            setDataAdapter();
+            this.tipoAcao = TipoAcao.taBrowse;
+        }
+
+        private void setDataAdapter()
+        {
+            SqlController sqlController = new SqlController();            
+            switch (tipoCadastro)
+            {
+                case TipoCadastro.tcServico:
+                    dataAdapter = sqlController.GetDataAdapter("SELECT * FROM servico");
+                    tabela = "servico";
+                    break;
+
+                case TipoCadastro.tcPagamento:
+                    dataAdapter = sqlController.GetDataAdapter("SELECT * FROM tipo_pagamento");
+                    tabela = "tipo_pagamento";
+                    break;
+
+                case TipoCadastro.tcAtendimento:
+                    dataAdapter = sqlController.GetDataAdapter("SELECT * FROM tipo_atendimento");
+                    tabela = "tipo_atendimento";
+                    break;
+
+                default:
+                    
+                    break;
+            }
+            dataSet.Clear();
+            grdDados.AutoGenerateColumns = true;
+            dataAdapter.Fill(dataSet, tabela);                
+            bindingSourceCadastro.DataSource = dataSet.Tables[tabela];
+            grdDados.DataSource = bindingSourceCadastro;
+            txtValor.Visible = this.tipoCadastro == TipoCadastro.tcServico;
+            lblValor.Visible = this.tipoCadastro == TipoCadastro.tcServico;
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            this.tipoAcao = TipoAcao.taIncluindo;
+            setEstadoCampos(true, true);
+        }
+
+        private void setEstadoCampos(Boolean ativo, Boolean limparCampos)
+        {
+            txtValor.Enabled = ativo;
+            txtDescricao.Enabled = ativo;         
+            if (limparCampos)
+            {
+                txtValor.Text = string.Empty; 
+                txtDescricao.Text = string.Empty;
+            }
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            this.tipoAcao = TipoAcao.taAlterando;
+            setEstadoCampos(true, false);
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            if (this.tipoAcao == TipoAcao.taBrowse)
+                this.Close();
+            else
+            {
+                this.tipoAcao = TipoAcao.taBrowse;
+                setEstadoCampos(false, true);
+            }
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {            
+            Salvar();
+            this.tipoAcao = TipoAcao.taBrowse;
+            setEstadoCampos(false, true);            
+            setDataAdapter();
+        }
+
+        private Boolean Salvar()
+        {
+            if (!ValidarCampos())
+            {
+                return false;
+            }
+            if (this.tipoAcao == TipoAcao.taIncluindo)
+            {
+                string insert;
+                if (tipoCadastro == TipoCadastro.tcServico)
+                {
+                    insert = "INSERT INTO "+tabela+" (descricao, valor) VALUES ('"+txtDescricao.Text+"', '"+txtValor.Text+"')";
+                }
+                else
+                {
+                    insert = "INSERT INTO "+tabela+" (descricao) VALUES ('" + txtDescricao.Text + "')";
+                }
+                SqlController sqlController = new SqlController();
+                sqlController.ExecSql(insert);
+            }
+            else if (this.tipoAcao == TipoAcao.taAlterando)
+            {
+                string campoValor = "";
+                if (tipoCadastro == TipoCadastro.tcServico)
+                {
+                    campoValor = ", valor = " + txtValor.Text;
+                }
+
+                DataGridViewRow linhaSelecionada = grdDados.SelectedRows[0];
+                string id = linhaSelecionada.Cells["id"].Value.ToString();
+                
+                string update = "UPDATE "+tabela+" SET descricao = '"+txtDescricao.Text+"' "+ campoValor + " WHERE ID = "+ id;
+
+                SqlController sqlController = new SqlController();
+                sqlController.ExecSql(update);
+            }
+
+            return true;
+        } 
+
+        private Boolean ValidarCampos()
+        {
+            if (txtValor.Visible)
+            {
+                if (txtValor.Text.Length == 0)
+                {
+                    MessageBox.Show("O Campo Valor não pode ser vazio!");
+                    return false;
+                }                
+            }
+
+            if (txtDescricao.Text.Length == 0)
+            {
+                MessageBox.Show("O Campo Descrição não pode ser vazio!");
+                return false;
+            }
+            return true;
+        }
+
+        private void grdDados_SelectionChanged(object sender, EventArgs e)
+        {
+            if (tipoAcao == TipoAcao.taBrowse)
+            {
+                DataGridView dataGridView = (DataGridView)sender;
+                if (dataGridView.SelectedRows.Count > 0)
+                {
+                    DataGridViewRow selectedRow = dataGridView.SelectedRows[0];
+                    if (txtValor.Visible)
+                    {
+                        txtValor.Text = selectedRow.Cells["valor"].Value.ToString().Replace(",", ".");
+                    }
+
+                    txtDescricao.Text = selectedRow.Cells["descricao"].Value.ToString();
+                }
+            }
+
+        }
+    }
+}
