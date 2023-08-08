c42354b670d74821fecc3fe67ea99c35ad0c0964
Luiz <Luizh.eng98@gmail.com>
2023-08-08 07:44:56 -0300

    Java-001 - Api de pagamentos. Até este commit, só tem o método post implementado

---
+++ ManagerService - Java/API/pagamento/src/main/java/com/manager/pagamento/service/PagamentoService.java
@@ -0,0 +1,27 @@
+package com.manager.pagamento.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.manager.pagamento.converter.PagamentoConverter;
+import com.manager.pagamento.dto.PagamentoPostDto;
+import com.manager.pagamento.entity.Pagamento;
+import com.manager.pagamento.repository.PagamentoRepository;
+
+@Service
+public class PagamentoService {
+		
+	private final PagamentoRepository repository;
+	
+	private final PagamentoConverter converter;
+	
+	@Autowired
+	public PagamentoService(PagamentoRepository repository, PagamentoConverter converter) {
+		this.repository = repository;
+		this.converter = converter;
+	}
+	
+	public Pagamento salvarPagamento(PagamentoPostDto dto) {
+		return repository.save(converter.mapToEntPost(dto));
+	}
+}
