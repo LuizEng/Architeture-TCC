1a17d2f5f05bc811d5a350caf7f6098d4a5c9225
Luiz <Luizh.eng98@gmail.com>
2023-08-12 14:23:18 -0300

    Dev-C#016 - Adicionado pacotes RabbitMQ e Listener para a Entidade Cliente

---
+++ ManagerService - Versão Inicial/ManagerService/ManagerService/RabbitMQ/RabbitMQListenerCliente.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ManagerService.Model.Dto;
+using Newtonsoft.Json;
+using ManagerService.Controller;
+using ManagerService.Service;
+using System.Windows.Forms;
+
+namespace ManagerService.RabbitMQ
+{
+    internal class RabbitMQListenerCliente : RabbitMQService
+    {
+        public void ListenForMessages()
+        {   
+            string retorno = ReceiveMessage("fila_cliente");
+            if (retorno != null)
+            {
+                ClientePostDto cliente = JsonConvert.DeserializeObject<ClientePostDto>(retorno);
+                ClienteService clienteService = new ClienteService();
+                clienteService.InserirCliente(cliente);
+            }
+        }
+    }
+}
